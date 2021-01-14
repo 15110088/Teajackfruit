e4dc3672a991c815023497c62d5f565def2a45b5
dinh tho <dinhtho@dinhs-iMac.local>
2021-01-14 08:35:39 +0700

    aa

--- src/screens/HomeScreen/Home.js
+++ src/screens/HomeScreen/Home.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,SafeAreaView } from 'react-native';
+import { Button } from 'react-native-paper';
+import ArcgisNghia ,{RNMapViewNative} from 'react-native-arcgis-nghia';
+var LAYERS = [
+  {
+    type: 'ArcGISTiledMapServiceLayer',
+    url: 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer',
+    username:'dothibienhoa',
+    password:'dothibienhoa2020'
 
+  },
+  {
+    type: 'ArcGISFeatureLayer',
+    url: 'https://services.arcgis.com/nzS0F0zdNLvs7nc8/arcgis/rest/services/MTBENCHMARK2DATA2/FeatureServer/0'
+  }
+];
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
     };
   }
-
+a = (e)=>{
+ console.log(e)
+}
   render() {
     return (
-      <View>
+      <SafeAreaView>
         <Text> Home nghia </Text>
-      </View>
+        {/* <Button onPress={()=>{ArcgisNghia.sampleMethod('123',2,this.a)}}>123</Button> */}
+        {/* <RNMapViewNative
+          layers={LAYERS} 
+          ref={ref => this.ref = ref}
+          style={{width:200,height:200}} /> */}
+          <Text style={{width:200,height:200,backgroundColor:'#ff0000'}}>12312</Text>
+      </SafeAreaView>
     );
   }
 }
